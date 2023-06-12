f01b8ed6804e6df87ce46e2b905b894c9c1ee721
Gabriel-GFF <gabrielfariasformal@gmail.com>
2023-06-12 13:45:46 -0300

    Carregado App base

---
+++ lista_chat_gpt/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TodoApp());
+}
+
+class TodoApp extends StatefulWidget {
+  @override
+  _TodoAppState createState() => _TodoAppState();
+}
+
+class _TodoAppState extends State<TodoApp> {
+  List<String> tasks = [];
+
+  void addTask(String task) {
+    setState(() {
+      tasks.add(task);
+    });
+  }
+
+  void completeTask(int index) {
+    setState(() {
+      tasks.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lista de Tarefas',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Lista de Tarefas'),
+        ),
+        body: ListView.builder(
+          itemCount: tasks.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(tasks[index]),
+              trailing: IconButton(
+                icon: Icon(Icons.check),
+                onPressed: () {
+                  completeTask(index);
+                },
+              ),
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                TextEditingController taskController = TextEditingController();
+
+                return AlertDialog(
+                  title: Text('Adicionar Tarefa'),
+                  content: TextField(
+                    controller: taskController,
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text('Cancelar'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    TextButton(
+                      child: Text('Adicionar'),
+                      onPressed: () {
+                        addTask(taskController.text);
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
